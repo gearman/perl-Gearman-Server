0c6aa6dd22a86f649997575be249f26a1317c9a7
Alexei Pastuchov <info@maximka.de>
2016-08-07 22:00:27 +0200

    Gearman::Server::Job pod

--- lib/Gearman/Server/Job.pm
+++ lib/Gearman/Server/Job.pm
@@ -5,21 +5,37 @@ $Gearman::Server::Job::VERSION = qv("v1.130.2");
 use strict;
 use warnings;
 
+=head1 NAME
+
+Gearman::Server::Job - job representation of L<Gearman::Server>
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=cut
+
 use Gearman::Server::Client;
 use Scalar::Util;
 use Sys::Hostname;
 
 use fields (
-            'func',
-            'uniq',
-            'argref',
-            'listeners',  # arrayref of interested Clients
-            'worker',
-            'handle',
-            'status',  # [1, 100]
-            'require_listener',
-            'server',  # Gearman::Server that owns us
-            );
+    'func',
+    'uniq',
+    'argref',
+
+    # arrayref of interested Clients
+    'listeners',
+    'worker',
+    'handle',
+
+    # [1, 100]
+    'status',
+    'require_listener',
+
+    # Gearman::Server that owns us
+    'server',
+);
 
 sub new {
     my Gearman::Server::Job $self = shift;
@@ -30,69 +46,100 @@ sub new {
     # if they specified a uniq, see if we have a dup job running already
     # to merge with
     if (length($uniq)) {
+
         # a unique value of "-" means "use my args as my unique key"
         $uniq = $$argref if $uniq eq "-";
         if (my $job = $server->job_of_unique($func, $uniq)) {
+
             # found a match
             return $job;
         }
+
         # create a new key
         $server->set_unique_job($func, $uniq => $self);
-    }
+    } ## end if (length($uniq))
 
-    $self->{'server'} = $server;
-    $self->{'func'}   = $func;
-    $self->{'uniq'}   = $uniq;
-    $self->{'argref'} = $argref;
+    $self->{'server'}           = $server;
+    $self->{'func'}             = $func;
+    $self->{'uniq'}             = $uniq;
+    $self->{'argref'}           = $argref;
     $self->{'require_listener'} = 1;
-    $self->{'listeners'} = [];
-    $self->{'handle'}  = $server->new_job_handle;
+    $self->{'listeners'}        = [];
+    $self->{'handle'}           = $server->new_job_handle;
 
     $server->enqueue_job($self, $highpri);
     return $self;
-}
+} ## end sub new
+
+=head2 add_listener($client)
+
+=cut
 
 sub add_listener {
-    my Gearman::Server::Job $self = shift;
+    my Gearman::Server::Job $self  = shift;
     my Gearman::Server::Client $li = shift;
 
-    push @{$self->{listeners}}, $li;
+    push @{ $self->{listeners} }, $li;
     Scalar::Util::weaken($self->{listeners}->[-1]);
-}
+} ## end sub add_listener
+
+=head2 relay_to_listeners($msg)
+
+=cut
 
 sub relay_to_listeners {
     my Gearman::Server::Job $self = shift;
-    foreach my Gearman::Server::Client $c (@{$self->{listeners}}) {
+    foreach my Gearman::Server::Client $c (@{ $self->{listeners} }) {
         next if !$c || $c->{closed};
         $c->write($_[0]);
     }
-}
+} ## end sub relay_to_listeners
+
+=head2 relay_to_option_listeners($msg, [$option])
+
+=cut
 
 sub relay_to_option_listeners {
     my Gearman::Server::Job $self = shift;
     my $option = $_[1];
-    foreach my Gearman::Server::Client $c (@{$self->{listeners}}) {
+    foreach my Gearman::Server::Client $c (@{ $self->{listeners} }) {
         next if !$c || $c->{closed};
         next unless $c->option($option);
         $c->write($_[0]);
     }
 
-}
+} ## end sub relay_to_option_listeners
+
+=head2 clear_listeners()
+
+=cut
 
 sub clear_listeners {
     my Gearman::Server::Job $self = shift;
     $self->{listeners} = [];
 }
 
+=head2 listeners()
+
+=cut
+
 sub listeners {
     my Gearman::Server::Job $self = shift;
-    return @{$self->{listeners}};
+    return @{ $self->{listeners} };
 }
+
+=head2 uniq()
+
+=cut
 
 sub uniq {
     my Gearman::Server::Job $self = shift;
     return $self->{uniq};
 }
+
+=head2 note_finished($success)
+
+=cut
 
 sub note_finished {
     my Gearman::Server::Job $self = shift;
@@ -103,7 +150,11 @@ sub note_finished {
     if ($Gearmand::graceful_shutdown) {
         Gearmand::shutdown_if_calm();
     }
-}
+} ## end sub note_finished
+
+=head2 worker()
+
+=cut
 
 # accessors:
 sub worker {
@@ -111,11 +162,20 @@ sub worker {
     return $self->{'worker'} unless @_;
     return $self->{'worker'} = shift;
 }
+
+=head2 require_listener([$require])
+
+=cut
+
 sub require_listener {
     my Gearman::Server::Job $self = shift;
     return $self->{'require_listener'} unless @_;
     return $self->{'require_listener'} = shift;
 }
+
+=head2 status([numerator,denominator])
+
+=cut
 
 # takes arrayref of [numerator,denominator]
 sub status {
@@ -124,15 +184,27 @@ sub status {
     return $self->{'status'} = shift;
 }
 
+=head2 handle()
+
+=cut
+
 sub handle {
     my Gearman::Server::Job $self = shift;
     return $self->{'handle'};
 }
 
+=head2 func()
+
+=cut
+
 sub func {
     my Gearman::Server::Job $self = shift;
     return $self->{'func'};
 }
+
+=head2 argref()
+
+=cut
 
 sub argref {
     my Gearman::Server::Job $self = shift;
